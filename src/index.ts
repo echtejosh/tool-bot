74caa1de275022c9cb2fdc11616c4b295d07e46e
mochidochi <ja.agripo2003@gmail.com>
2023-09-15 04:49:00 +0200

    Added banner for README.md

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ import { MusicService } from './services/MusicService';
 import DisTube from 'distube';
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildVoiceStates,
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildModeration,
+		GatewayIntentBits.GuildVoiceStates,
+	],
 });
 
 const distube = new DisTube(client);
@@ -24,33 +24,33 @@ const databaseService = new DatabaseService();
 import * as events from './events';
 
 const eventService = new EventService({
-    events: [
-        events.interactionCreate,
-        events.messageCreate,
-        events.ready,
-    ],
+	events: [
+		events.interactionCreate,
+		events.messageCreate,
+		events.ready,
+	],
 });
 
 const musicService = new MusicService({
-    events: [
-        events.addSong,
-        events.addList,
-    ],
-    client,
-    distube,
+	events: [
+		events.addSong,
+		events.addList,
+	],
+	client,
+	distube,
 });
 
 import * as commands from './commands';
 
 const commandService = new CommandService({
-    commands: Object.values(commands),
+	commands: Object.values(commands),
 });
 
 const bot = new Bot({
-    client,
-    eventService,
-    commandService,
-    musicService,
+	client,
+	eventService,
+	commandService,
+	musicService,
 });
 
 databaseService.connect(process.env.MONGO_URI!);
