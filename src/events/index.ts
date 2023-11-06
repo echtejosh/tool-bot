8bcac15f35bd070feb423d7c2ab12c4a042dda0a
echtyushi <ja.agripo2003@gmail.com>
2023-11-06 02:15:13 +0100

    Tidied up some unnecessary code

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,5 @@
-export * from './discord/interactionCreate';
-export * from './discord/messageCreate';
-export * from './discord/ready';
-export * from './distube/addSong';
-export * from './distube/addList';
+export { interactionCreate } from './discord/interactionCreate';
+export { messageCreate } from './discord/messageCreate';
+export { ready } from './discord/ready';
+export { addSong } from './distube/addSong';
+export { addList } from './distube/addList';
